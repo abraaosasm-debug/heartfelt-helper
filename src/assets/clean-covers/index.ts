e54492dcab3cee2cbdfd81dac43ca0ddb5e1c3b2
abraaosasm-debug <abraaos.asm@gmail.com>
2026-09-22 11:10:37 -0500

    Encapsulate clean cover image data

---
+++ src/assets/clean-covers/index.ts
@@ -0,0 +1,16 @@
+import cover10 from "./cover1-0";
+import cover11 from "./cover1-1";
+import cover12 from "./cover1-2";
+import cover13 from "./cover1-3";
+import cover70 from "./cover7-0";
+import cover71 from "./cover7-1";
+import cover72 from "./cover7-2";
+import cover73 from "./cover7-3";
+import cover74 from "./cover7-4";
+import cover75 from "./cover7-5";
+
+export const cleanCover1 =
+  "data:image/avif;base64," + [cover10, cover11, cover12, cover13].join("");
+
+export const cleanCover7 =
+  "data:image/avif;base64," + [cover70, cover71, cover72, cover73, cover74, cover75].join("");
